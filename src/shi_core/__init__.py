ef6fbc94a7c88b2dbd004f3d785faf930073d95b
Beltran <jlbeltran930128@gmail.com>
2026-04-07 11:31:15 +0200

    .

--- src/shi_core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""SHI core package initialization."""
-from .processor import SHIProcessor
-from .cleaner import Cleaner
-from .exceptions import SHIError
-from .config import config
-
-__all__ = ["SHIProcessor", "Cleaner", "SHIError", "config"]
